f3953c515a4e642ef2a0d81b240eba364bdac5ea
Emilie Liu <emilielena@github.com>
2018-02-02 02:30:58 +0100

    Version 1.0
    First attempt

---
+++ function.js
@@ -0,0 +1,10 @@
+/*javascript document*/
+/* ARCHE DE NOE interactif */
+var x = 0;
+var y = 0;
+var z = 0;
+
+function myMoveFunction() {
+    document.getElementById("Counter").innerHTML = z+=1;
+}
+
